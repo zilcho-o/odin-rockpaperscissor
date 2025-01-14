11d3b3944a5fc44d46f61dfc5cf3da1a52519c87
zilchox <164333718+zilcho-o@users.noreply.github.com>
2025-01-14 12:50:39 +0800

    Created function to get computer's choice

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+function getComputerChoice() {
+    choice = Math.random();
+    if (choice <=0.3) {
+        return "rock";
+    }
+    else if (choice <=0.6) {
+        return "paper";
+    }
+    return "scissors";
+}
+
+console.log(getComputerChoice())
